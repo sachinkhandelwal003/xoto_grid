f2a5b0a683344223a82e9f04b5224152400d480a
sachinkhandelwal003 <sachinkhandelwal003@example.com>
2026-06-11 15:03:11 +0530

    adfa

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import { showToast } from '../utils/toast';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
-  ? `${import.meta.env.VITE_API_BASE_URL}/api/`
-  : 'https://xoto.ae/api/';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
 
 const api = axios.create({ baseURL: API_BASE_URL });
 
